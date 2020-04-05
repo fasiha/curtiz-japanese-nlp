7f8b87dbe092293054987b0c12f628897c64036b
Ahmed Fasih <wuzzyview@gmail.com>
2020-04-05 18:42:11 -0400

    about to refactor annotate Json output

--- jsonAnnotate.ts
+++ jsonAnnotate.ts
@@ -2,17 +2,18 @@ import {dedupe, flatten} from 'curtiz-utils';
 
 import {analyzeSentence, getTags, scoreHitsToWords} from './annotate';
 
-(
-    async function main() {
-      const sentences = `やる事明確
+if (module === require.main) {
+  const raw = `やる事明確
 少しでも良くするこの生活
 勝ち抜くTry Out
-Rap Game 頂く王冠`.trim().split('\n');
-      const overrides = new Map();
-      const analysis = await Promise.all(sentences.map(s => analyzeSentence(s, overrides)));
-      const words =
-          dedupe(flatten(await Promise.all(analysis.map(o => scoreHitsToWords(flatten(flatten(o.dictionaryHits)))))),
-                 o => o.id);
-      const tags = getTags();
-      console.log(JSON.stringify({analysis, words, tags}, null, 1));
-    })();
+Rap Game 頂く王冠`;
+  const sentences = raw.trim().split('\n');
+  (async function main() {
+    const overrides = new Map();
+    const analysis = await Promise.all(sentences.map(s => analyzeSentence(s, overrides)));
+    const words = dedupe(
+        flatten(await Promise.all(analysis.map(o => scoreHitsToWords(flatten(flatten(o.dictionaryHits)))))), o => o.id);
+    const tags = getTags();
+    console.log(JSON.stringify({analysis, words, tags}, null, 1));
+  })();
+}
